75c461763294bb14b8995341a450e1493f3f0e93
Ihor  Zaviriukha <igor.zaviriukha@onix-systems.com>
2021-04-14 15:21:14 +0300

    added AppProviders

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import { MuiThemeProvider } from "@material-ui/core/styles";
 import { Routes } from "Routes";
 import { appTheme } from "theme";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { Router } from "react-router-dom";
 import { appHistory } from "appHistory";
-import { Provider } from "react-redux";
 import { store } from "store";
+import { AppProviders } from "AppProviders";
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <MuiThemeProvider theme={appTheme}>
-        <CssBaseline />
-        <Router history={appHistory}>
-          <Routes />
-        </Router>
-      </MuiThemeProvider>
-    </Provider>
+    <AppProviders theme={appTheme} history={appHistory} store={store}>
+      <Routes />
+    </AppProviders>
   );
 };
 
---
+++ src/AppProviders.tsx
@@ -0,0 +1,29 @@
+import { Router, RouterProps } from "react-router";
+import { Provider } from "react-redux";
+import { RootState } from "store";
+import React from "react";
+import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
+import { DefaultTheme } from "@material-ui/styles";
+import { Store } from "redux";
+
+type AppProvidersProps = {
+  children: React.ReactNode;
+  theme: DefaultTheme;
+  history: RouterProps["history"];
+  store: Store<RootState>;
+};
+
+const AppProviders = (props: AppProvidersProps) => {
+  const { children, theme, history, store } = props;
+
+  return (
+    <Provider store={store}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router history={history}>{children}</Router>
+      </MuiThemeProvider>
+    </Provider>
+  );
+};
+
+export { AppProviders };
