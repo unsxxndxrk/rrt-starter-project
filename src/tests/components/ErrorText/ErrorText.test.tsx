539287828d84631e67aff96150d20e7746b89266
Ihor  Zaviriukha <igor.zaviriukha@onix-systems.com>
2021-06-08 17:50:26 +0300

    RRTSP-1: Added tests for ErrorText component

--- src/tests/components/ErrorText/ErrorText.test.tsx
+++ src/tests/components/ErrorText/ErrorText.test.tsx
@@ -2,6 +2,7 @@ import { render } from "tests/settings/render";
 import ErrorText from "components/ErrorText/ErrorText";
 
 const errorTextMock: string = "error_mock";
+const classNameMock: string = "class_name_mock";
 
 describe("<ErrorText />", () => {
   test("should render ErrorText component with custom text", () => {
@@ -14,5 +15,23 @@ describe("<ErrorText />", () => {
     const wrapper = render(<ErrorText type="unexpected" />);
     expect(wrapper.asFragment()).toMatchSnapshot();
     expect(wrapper.getByText("errors.unexpected")).toBeTruthy();
+  });
+
+  test("should render ErrorText component with custom className", () => {
+    const wrapper = render(
+      <ErrorText text={errorTextMock} className={classNameMock} />,
+    );
+    expect(wrapper.asFragment()).toMatchSnapshot();
+    expect(wrapper.container.getElementsByClassName(classNameMock).length).toBe(
+      1,
+    );
+  });
+
+  test("should render ErrorText component with variant=tiny", () => {
+    const wrapper = render(<ErrorText text={errorTextMock} variant="tiny" />);
+    expect(wrapper.asFragment()).toMatchSnapshot();
+    expect(
+      wrapper.container.getElementsByClassName("makeStyles-tiny").length,
+    ).toBe(1);
   });
 });
