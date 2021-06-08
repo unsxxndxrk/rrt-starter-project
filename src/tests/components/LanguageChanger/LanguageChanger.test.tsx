d6b87bb84d553998ced441e587038bdc20e9f940
Ihor  Zaviriukha <igor.zaviriukha@onix-systems.com>
2021-06-08 18:33:35 +0300

    RRTSP-1: Added test for LanguageChanger component

--- src/tests/components/LanguageChanger/LanguageChanger.test.tsx
+++ src/tests/components/LanguageChanger/LanguageChanger.test.tsx
@@ -1,9 +1,22 @@
 import { render } from "tests/settings/render";
 import LanguageChanger from "components/LanguageChanger/LanguageChanger";
+import { RenderResult } from "@testing-library/react";
+
+let wrapper: RenderResult;
+
+beforeEach(() => {
+  wrapper = render(<LanguageChanger />);
+});
 
 describe("<LanguageChanger />", () => {
   test("should render LanguageChanger component", () => {
-    const wrapper = render(<LanguageChanger />);
     expect(wrapper.asFragment()).toMatchSnapshot();
+  });
+
+  test("should have language buttons", () => {
+    const enButton = wrapper.getByText("en");
+    const ruButton = wrapper.getByText("ru");
+    expect(enButton).toBeInTheDocument();
+    expect(ruButton).toBeInTheDocument();
   });
 });
